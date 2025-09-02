12405da41854800f9207fc906df4c8cbe3cd0b82
Jay <rright815@gmail.com>
2025-09-03 00:32:38 +0800

    116

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,19 @@ import { Component, input, Input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
+//@HostBinding('class') className = 'control';
+// @HostListener('click')onClick(){
+//   console.log('clicked!')
+// }
 label = input.required<string>();
+
+onClick(){
+   console.log('clicked!')
+ }
+
 }
