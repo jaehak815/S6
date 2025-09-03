58f71271ba31036ee68e25ad6e8d76bca4c0e789
Jay <rright815@gmail.com>
2025-09-03 21:48:30 +0800

    133

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,15 @@
-import { Component, ElementRef, HostBinding, HostListener, inject, input, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  contentChild,
+  ContentChild,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,20 +20,25 @@ import { Component, ElementRef, HostBinding, HostListener, inject, input, Input,
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
-    '(click)': 'onClick()'
-  }
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
-//@HostBinding('class') className = 'control';
-// @HostListener('click')onClick(){
-//   console.log('clicked!')
-// }
-label = input.required<string>();
-private el = inject(ElementRef)
+  //@HostBinding('class') className = 'control';
+  // @HostListener('click')onClick(){
+  //   console.log('clicked!')
+  // }
+  label = input.required<string>();
+  private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+  private control =
+    contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
 
-onClick(){
-   console.log('clicked!');
-   console.log(this.el);
- }
-
+  onClick() {
+    console.log('clicked!');
+    console.log(this.el);
+    console.log(this.control());
+  }
 }
